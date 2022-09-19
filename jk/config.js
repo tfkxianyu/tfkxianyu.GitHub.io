0c31cbcd7b6470cce1d90cd0916efa2a866fbd06
tfkxianyu <106020990+tfkxianyu@users.noreply.github.com>
2022-09-19 12:36:45 +0800

    Update config.js

--- jk/config.js
+++ jk/config.js
@@ -1,12 +1,15 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'TFK-云监控',
+  SiteName: 'Public Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1861475-6904ccae3b5f32875d63c755',
+    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
@@ -18,9 +21,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'TFK截短云',
-      url: 'http://tc.jieduan.fun/'
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
     },
   ],
 };
-
